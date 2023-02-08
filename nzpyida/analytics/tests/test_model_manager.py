096f54e4ced1cf95f5d08ff1329025316fd2d74a
Marcin Labenski <marcin.labenski@pl.ibm.com>
2023-02-08 16:14:00 +0100

    Unit test fixes related to the last refactoring

--- nzpyida/analytics/tests/test_model_manager.py
+++ nzpyida/analytics/tests/test_model_manager.py
@@ -8,15 +8,15 @@
 #
 # The full license is in the LICENSE file, distributed with this software.
 #-----------------------------------------------------------------------------
-
+import pytest
 from nzpyida.frame import IdaDataFrame
 from nzpyida.base import IdaDataBase
 from nzpyida.analytics.model_manager import ModelManager
-from nzpyida.analytics.predictive.decision_trees import DecisionTreeClassifier
+from nzpyida.analytics.predictive.regression_trees import DecisionTreeRegressor
+import pandas as pd
 
-import pytest
-
-MOD_NAME = "TEST_MOD1"
+MOD_NAME = 'TEST_MOD1'
+TAB_NAME = 'TEST_TABLE1'
 
 @pytest.fixture(scope='module')
 def mm(idadb: IdaDataBase):
@@ -24,12 +24,15 @@ def mm(idadb: IdaDataBase):
 
 @pytest.fixture(scope='module')
 def create_delete_model(idadb: IdaDataBase):
-    df = IdaDataFrame(idadb, 'TRAINING_DATA2', indexer='IMSI')
-    DecisionTreeClassifier(idadb, model_name=MOD_NAME).fit(df.loc[:10], id_column='IMSI', target_column='IS_FRAUD')
+    pdf = pd.DataFrame.from_dict({'ID': [0,2,4], 'X': [0,2,4], 'Y': [0, 4, 8]})
+    df = idadb.as_idadataframe(pdf, tablename=TAB_NAME, clear_existing=True)
+    DecisionTreeRegressor(idadb, model_name=MOD_NAME).fit(df, id_column='ID', target_column='Y')
     yield
     ret = idadb.ida_query(f'call NZA..MODEL_EXISTS(\'model={MOD_NAME}\')')
     if not ret.empty and ret[0]:
         idadb.ida_query(f'call NZA..DROP_MODEL(\'model={MOD_NAME}\')')
+    if idadb.exists_table(TAB_NAME):
+        idadb.drop_table(TAB_NAME)
 
 
 def test_list_models(mm: ModelManager):
